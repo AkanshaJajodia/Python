8c74acf163319bb5778e99488776362fd249e1b4
AkanshaJajodia <55075215+AkanshaJajodia@users.noreply.github.com>
2020-02-28 13:13:14 -0800

    Delete printingfileinfolder.py

--- printingfileinfolder.py
+++
@@ -1,8 +0,0 @@
-import os, glob
-
-os.chdir("/Users/akanshajajodia/Desktop")
-count = 0
-for file in glob.glob("*.png"):
-    print(file)
-    count +=1
-print("Total number of PNG files in this folder is ", count)
